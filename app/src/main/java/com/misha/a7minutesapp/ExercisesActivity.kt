fe8c82116017964ba9883e203f468b87208fae5e
wet-creation <91834478+wet-creation@users.noreply.github.com>
2023-01-12 19:09:52 +0200

    Text to speech implemented

--- app/src/main/java/com/misha/a7minutesapp/ExercisesActivity.kt
+++ app/src/main/java/com/misha/a7minutesapp/ExercisesActivity.kt
@@ -4,16 +4,21 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.misha.a7minutesapp.databinding.ActivityExercisesBinding
 import com.misha.a7minutesapp.model.ExercisesModel
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExercisesActivity : AppCompatActivity() {
+class ExercisesActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
     private var exercisesList = ArrayList<ExercisesModel>()
     private var currentExercises = 0
+    private lateinit var tts:TextToSpeech
 
 
     private var binding: ActivityExercisesBinding? = null
@@ -23,8 +28,9 @@ class ExercisesActivity : AppCompatActivity() {
         setContentView(binding?.root)
         setSupportActionBar(binding?.toolbarExercise)
         exercisesList = Constants.defaultExercisesList()
-        setUpRestView()
+        tts = TextToSpeech(this, this)
 
+        setUpRestView()
         if (supportActionBar != null){
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
@@ -46,6 +52,7 @@ class ExercisesActivity : AppCompatActivity() {
         binding?.tvNameOfExercise?.visibility = View.VISIBLE
         binding?.tvTimerText?.text = "GET READY FOR"
         binding?.tvNameOfExercise?.text = exercisesList[currentExercises].name
+        speak("GET READY FOR ${exercisesList[currentExercises].name}")
         binding?.progressBar?.max = 10
         setRestProgressBar(10)
     }
@@ -58,6 +65,7 @@ class ExercisesActivity : AppCompatActivity() {
         binding?.tvNameOfExercise?.visibility = View.INVISIBLE
         binding?.imgExercise?.visibility = View.VISIBLE
         binding?.imgExercise?.setImageResource(exercisesList[currentExercises].image)
+        speak(exercisesList[currentExercises].name)
         binding?.progressBar?.max = 30
         setRestProgressBar(30)
     }
@@ -68,6 +76,10 @@ class ExercisesActivity : AppCompatActivity() {
         restTimer = object : CountDownTimer(tick * time, tick){
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
+                if (time.toInt() - restProgress <= 3){
+                    if (time.toInt() - restProgress != 0)
+                        speak("${time.toInt() - restProgress}")
+                }
                 binding?.progressBar?.progress = time.toInt() - restProgress
                 binding?.timer?.text = "${time - restProgress}"
             }
@@ -92,13 +104,30 @@ class ExercisesActivity : AppCompatActivity() {
         }.start()
 
     }
-
+    private fun speak(string: String){
+        tts.speak(string, TextToSpeech.QUEUE_ADD,null,"")
+    }
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts.setLanguage(Locale.ENGLISH)
+            speak("GET READY FOR ${exercisesList[currentExercises].name}")
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED)
+                Log.e("TTS", "The language is not supported!")
+        }
+        else Log.e("TTS", "The initialization failed")
+    }
     override fun onDestroy() {
         super.onDestroy()
         if (restTimer!=null){
             restTimer?.cancel()
             restProgress = 0
         }
+        if (tts.isSpeaking){
+            tts.stop()
+            tts.shutdown()
+        }
         binding = null
     }
+
+
 }
